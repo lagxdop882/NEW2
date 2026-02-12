02a8d065d09fd0296dc0b6b12f63efc21760b7aa
lagxdop882 <155067717+lagxdop882@users.noreply.github.com>
2026-02-12 08:31:12 +0530

    Change bot token in loader.py
    Updated the bot token for the Telegram bot.

--- loader.py
+++ loader.py
@@ -1,6 +1,6 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-BOT_TOKEN = '5836125737:AAEMvAxSeKA6O6ViOuay68v-u1suJ_IhfI'
+BOT_TOKEN = '7961200564:AAHx0T0k_dx_DkKKJjnDoG0jESXFb_OHfOo'
 bot = Bot(token=BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
